e9c3436c9aa839f1378ae1e9535d03736c2413dc
ZhangHong-Rui <56534744+ZhangHong-Rui@users.noreply.github.com>
2023-06-10 11:06:35 +0800

    Update deleteAndEarn.py

--- deleteAndEarn.py
+++ deleteAndEarn.py
@@ -1,16 +1,15 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
+
         maxVal = max(nums)
         total = [0] * (maxVal + 1)
         for val in nums:
             total[val] += val
-        
+
         def rob(nums: List[int]) -> int:
             size = len(nums)
-            first, second = nums[0], max(nums[0], nums[1])
+            dp = [nums[0], max(nums[0], nums[1])]
             for i in range(2, size):
-                first, second = second, max(first + nums[i], second)
-            return second
-        
+                dp.append(max(dp[i-2]+nums[i],dp[i-1]))
+            return dp[-1]
         return rob(total)
-
