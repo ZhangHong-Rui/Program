dea726a117c7c42539a4ba5dda5823d6758ea029
ZhangHong-Rui <56534744+ZhangHong-Rui@users.noreply.github.com>
2023-06-12 23:18:29 +0800

    Update uniquePaths.py

--- uniquePaths.py
+++ uniquePaths.py
@@ -3,3 +3,29 @@
     '''
        which leads to a proble about the computation-measurment about C, C m-1(m+n-2) = fac(m+n-2)/fac(n-1)/fac(m-1)
     '''
+    
+        def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1]*n] + [[1]+[0] * (n-1) for _ in range(m-1)]
+        #print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
+ def uniquePaths(self, m: int, n: int) -> int:
+        pre = [1] * n
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] = pre[j] + cur[j-1]
+            pre = cur[:]
+        return pre[-1]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
+
+
